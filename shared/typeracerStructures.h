2bb4249318d8cfaf7fb1d39a0e9ba77cc3f538ab
Samuel Fábry <fexos987@gmail.com>
2025-01-12 14:54:48 +0100

    Edit typeracerStructures

--- shared/typeracerStructures.h
+++ shared/typeracerStructures.h
@@ -7,43 +7,78 @@
 #include <pthread.h>
 #include <sys/time.h>
 
-typedef struct Player {
+typedef struct Player
+{
     int activeSocket;
-    char* name;
-    int currentWordIndex;
-    struct timeval startTime, endTime;
+    char *name;
+    double timeTaken;
+    bool finished;
+    bool disconnected;
 } Player;
 
-typedef struct RefreshingData {
-    int* progress;
+typedef struct Times
+{
+    double *times;
+} Times;
+
+typedef struct RefreshingData
+{
+    int *progress;
 } RefreshingData;
 
-typedef struct SocketsData {
-    char** names;
-    char* sentence;
-    int wordsCount;
+typedef struct SocketsData
+{
+    char **names;
+    char *sentence;
     int playerCount;
+    int playerIndex;
 } SocketsData;
 
-typedef struct Game {
+typedef struct Game
+{
     int serverSocket;
-    Player** players;
-    int maxPlayerCount; //TODO: Change lobby master logic
+    Player **players;
+    int maxPlayerCount;
     int actualPlayerCount;
-    pthread_t* threads;
-    pthread_mutex_t* mutex;
-    pthread_cond_t* startCond;
-    pthread_cond_t* finishCond;
-    int wordsCount;
+    pthread_t *threads;
     int finishedPlayers;
-    SocketsData* socketsData;
-    RefreshingData* refreshingData;
+    SocketsData *socketsData;
+    RefreshingData *refreshingData;
 } Game;
 
-
-typedef struct ThreadData {
-    Game* game;
+typedef struct ThreadData
+{
+    Game *game;
+    RefreshingData *refreshingData;
+    pthread_mutex_t *mutex;
+    pthread_cond_t *startCond;
+    pthread_cond_t *finishCond;
+    pthread_cond_t *timeCond;
     int playerIndex;
 } ThreadData;
+
+typedef struct Client
+{
+    int activeSocket;
+    int playerIndex;
+    int currentWordIndex;
+    struct timeval startTime, endTime;
+    char *playerWord;
+    char **words;
+    int charsDone;
+    int wordCount;
+    char **playerNames;
+    int playerCount;
+} Client;
+
+typedef struct data
+{
+    int *progress;
+    Client *client;
+    pthread_mutex_t *mutex;
+    pthread_cond_t *wait;
+    pthread_cond_t *refresh;
+    bool serverDisconnected;
+} data;
 
 #endif // TYPERACERSTRUCTURES_H
