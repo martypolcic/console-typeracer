bfb8034c31a5c09a8843af576036faf7278dd640
Samuel Fábry <fexos987@gmail.com>
2025-01-12 14:54:18 +0100

    Edit jsonHandler

--- shared/jsonHandler.c
+++ shared/jsonHandler.c
@@ -1,60 +1,112 @@
 #include "jsonHandler.h"
 
-char* serializeRefreshingData(const RefreshingData* data, int playerCount) {
-    cJSON* json = cJSON_CreateObject();
-    cJSON* progressArray = cJSON_CreateIntArray(data->progress, playerCount);
+char *serializeRefreshingData(const RefreshingData *data, int playerCount)
+{
+    cJSON *json = cJSON_CreateObject();
+    cJSON *progressArray = cJSON_CreateIntArray(data->progress, playerCount);
     cJSON_AddItemToObject(json, "progress", progressArray);
-    char* jsonString = cJSON_Print(json);
+    char *jsonString = cJSON_Print(json);
     cJSON_Delete(json);
     return jsonString;
 }
 
-RefreshingData deserializeRefreshingData(const char* jsonString) {
+RefreshingData deserializeRefreshingData(const char *jsonString)
+{
     RefreshingData data;
-    cJSON* json = cJSON_Parse(jsonString);
-    cJSON* progressArray = cJSON_GetObjectItem(json, "progress");
+    cJSON *json = cJSON_Parse(jsonString);
+    cJSON *progressArray = cJSON_GetObjectItem(json, "progress");
     int progressArraySize = cJSON_GetArraySize(progressArray);
     data.progress = malloc(progressArraySize * sizeof(int));
-    for (int i = 0; i < progressArraySize; i++) {
-        cJSON* progressItem = cJSON_GetArrayItem(progressArray, i);
+    for (int i = 0; i < progressArraySize; i++)
+    {
+        cJSON *progressItem = cJSON_GetArrayItem(progressArray, i);
         data.progress[i] = progressItem->valueint;
     }
     cJSON_Delete(json);
     return data;
 }
 
-char* serializeSocketsData(const SocketsData* data) {
-    cJSON* json = cJSON_CreateObject();
-    cJSON* namesArray = cJSON_CreateStringArray((const char* const*)data->names, data->playerCount);
+char *serializeSocketsData(const SocketsData *data, const int playerIndex)
+{
+    cJSON *json = cJSON_CreateObject();
+    cJSON *namesArray = cJSON_CreateStringArray((const char *const *)data->names, data->playerCount);
     cJSON_AddItemToObject(json, "names", namesArray);
     cJSON_AddStringToObject(json, "sentence", data->sentence);
-    cJSON_AddNumberToObject(json, "wordsCount", data->wordsCount);
     cJSON_AddNumberToObject(json, "playerCount", data->playerCount);
-    char* jsonString = cJSON_Print(json);
+    cJSON_AddNumberToObject(json, "playerIndex", playerIndex);
+    char *jsonString = cJSON_Print(json);
     cJSON_Delete(json);
     return jsonString;
 }
 
-SocketsData deserializeSocketsData(const char* jsonString) {
+SocketsData deserializeSocketsData(const char *jsonString)
+{
     SocketsData data;
-    cJSON* json = cJSON_Parse(jsonString);
-    cJSON* namesArray = cJSON_GetObjectItem(json, "names");
-    if (namesArray != NULL) {
+    cJSON *json = cJSON_Parse(jsonString);
+    cJSON *namesArray = cJSON_GetObjectItem(json, "names");
+    if (namesArray != NULL)
+    {
         int namesArraySize = cJSON_GetArraySize(namesArray);
-        data.names = malloc(namesArraySize * sizeof(char*));
-        for (int i = 0; i < namesArraySize; i++) {
-            cJSON* nameItem = cJSON_GetArrayItem(namesArray, i);
+        data.names = malloc(namesArraySize * sizeof(char *));
+        for (int i = 0; i < namesArraySize; i++)
+        {
+            cJSON *nameItem = cJSON_GetArrayItem(namesArray, i);
             data.names[i] = strdup(nameItem->valuestring);
         }
     }
-    cJSON* sentenceItem = cJSON_GetObjectItem(json, "sentence");
-    if (sentenceItem != NULL) {
+    cJSON *sentenceItem = cJSON_GetObjectItem(json, "sentence");
+    if (sentenceItem != NULL)
+    {
         data.sentence = strdup(sentenceItem->valuestring);
     }
-    cJSON* wordsCountItem = cJSON_GetObjectItem(json, "wordsCount");
-    data.wordsCount = wordsCountItem->valueint;
-    cJSON* playerCountItem = cJSON_GetObjectItem(json, "playerCount");
+    cJSON *playerCountItem = cJSON_GetObjectItem(json, "playerCount");
     data.playerCount = playerCountItem->valueint;
+    cJSON *playerIndexItem = cJSON_GetObjectItem(json, "playerIndex");
+    data.playerIndex = playerIndexItem->valueint;
+    cJSON_Delete(json);
+    return data;
+}
+
+int calculateRefreshingDataSize(int playerCount, int wordCount)
+{
+    int *progress = calloc(playerCount, sizeof(int));
+    for (int i = 0; i < playerCount; i++)
+    {
+        progress[i] = wordCount;
+    }
+
+    RefreshingData data;
+    data.progress = progress;
+
+    char *jsonString = serializeRefreshingData(&data, playerCount);
+    int size = strlen(jsonString);
+    free(progress);
+    free(jsonString);
+    return size;
+}
+
+char *serializeTimes(const Times *data, int playerCount)
+{
+    cJSON *json = cJSON_CreateObject();
+    cJSON *timesArray = cJSON_CreateDoubleArray(data->times, playerCount);
+    cJSON_AddItemToObject(json, "times", timesArray);
+    char *jsonString = cJSON_Print(json);
+    cJSON_Delete(json);
+    return jsonString;
+}
+
+Times deserializeTimes(const char *jsonString)
+{
+    Times data;
+    cJSON *json = cJSON_Parse(jsonString);
+    cJSON *timesArray = cJSON_GetObjectItem(json, "times");
+    int timesArraySize = cJSON_GetArraySize(timesArray);
+    data.times = malloc(timesArraySize * sizeof(double));
+    for (int i = 0; i < timesArraySize; i++)
+    {
+        cJSON *timeItem = cJSON_GetArrayItem(timesArray, i);
+        data.times[i] = timeItem->valuedouble;
+    }
     cJSON_Delete(json);
     return data;
 }
--- shared/jsonHandler.h
+++ shared/jsonHandler.h
@@ -7,11 +7,18 @@
 #include <string.h>
 
 // Function prototypes for RefreshingData
-char* serializeRefreshingData(const RefreshingData* data, int playerCount);
-RefreshingData deserializeRefreshingData(const char* jsonString);
+char *serializeRefreshingData(const RefreshingData *data, int playerCount);
+RefreshingData deserializeRefreshingData(const char *jsonString);
 
 // Function prototypes for SocketsData
-char* serializeSocketsData(const SocketsData* data);
-SocketsData deserializeSocketsData(const char* jsonString);
+char *serializeSocketsData(const SocketsData *data, const int playerIndex);
+SocketsData deserializeSocketsData(const char *jsonString);
+
+// Function prototypes for calculating the size of the RefreshingData
+int calculateRefreshingDataSize(int playerCount, int wordCount);
+
+// Function protorypes for Time struct
+char *serializeTimes(const Times *data, int playerCount);
+Times deserializeTimes(const char *jsonString);
 
 #endif // JSON_HANDLER_H
